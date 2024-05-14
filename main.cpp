1a32c6b058f3f0b32732c2e4b9049af6dfc281a2
WilliamNorboe <98242626+WilliamNorboe@users.noreply.github.com>
2024-05-14 02:11:57 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+int main() {
+    std::string str = "";
+    std::getline(std::cin, str);
+    std::stringstream stringStream1(str);
+    std::vector<int> result;
+    int value = 0;
+    while (stringStream1 >> value){
+        result.push_back(value);
+    }
+    if(result.size() != 4){
+        return -1;
+    }
+
+
+
+
+    return 0;
+}
